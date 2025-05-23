7d0e0065f5be9b5a60a601fd8b4889080ab03045
stephansama <stephanrandle@hotmail.com>
2025-05-23 00:03:17 -0400

    chore: removed test script

--- test.js
+++
@@ -1,6 +0,0 @@
-import url from "node:url";
-
-const pkg2 = new url.URL("./examples/ui", import.meta.url);
-const pkg = new url.URL("./examples/ui/package.json", import.meta.url);
-
-console.log(pkg, pkg2);
