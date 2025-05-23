5d399df1a5a21c2c0eb35911685132d228345f40
stephansama <stephanrandle@hotmail.com>
2025-05-23 00:10:35 -0400

    chore: fixed plugin to save to fs

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,8 +2,10 @@ import type { IconifyJSON } from "@iconify/types";
 import type { Plugin, ResolvedConfig } from "vite";
 
 import { loadCollectionFromFS } from "@iconify/utils/lib/loader/fs";
+import { iconToSVG } from "@iconify/utils/lib/svg/build";
 import { createHash } from "node:crypto";
-import { readFile } from "node:fs/promises";
+import fs from "node:fs";
+import path from "node:path";
 
 const PLUGIN_NAME = "vite-plugin-iconify-svgmap";
 const virtualModuleId = "virtual:iconify-svgmap";
@@ -13,11 +15,38 @@ const js = String.raw;
 
 export type Options = Partial<{
 	iconifyRootDirectory: URL;
+	outDir?: string;
 }>;
+
+const cacheFilename = path.resolve(
+	"node_modules/.vite/deps/iconifysvgmap.json",
+);
+
+export async function getIcon(pack: string, name: string) {
+	const current = fs.readFileSync(cacheFilename, {
+		encoding: "utf-8",
+		flag: "as+",
+	});
+
+	const currentRepresentation = JSON.parse(current || "{}");
+	const newPack = currentRepresentation[pack]?.includes(name)
+		? currentRepresentation[pack]
+		: [
+				...(currentRepresentation[pack]
+					? currentRepresentation[pack]
+					: []),
+				name,
+			];
+
+	const newRepresentation = { ...currentRepresentation, [pack]: newPack };
+
+	fs.writeFileSync(cacheFilename, JSON.stringify(newRepresentation));
+	return `/${pack}.svg#${name}`;
+}
 
 export default function createPlugin(options?: Options): Plugin {
 	let config: ResolvedConfig;
-	let collections: Record<string, IconifyJSON>;
+	let inMemoryCollections: Record<string, IconifyJSON> = {};
 
 	return {
 		name: PLUGIN_NAME,
@@ -31,15 +60,9 @@ export default function createPlugin(options?: Options): Plugin {
 			}
 		},
 
-		configureServer(server) {
-			server.middlewares.use((req, res, next) => {
-				// req.
-			});
-		},
-
 		async load(id) {
 			if (id !== resolvedVirtualModuleId) return;
-			const text = await readFile(
+			const text = fs.readFileSync(
 				new URL("./package.json", options?.iconifyRootDirectory),
 				{ encoding: "utf8" },
 			);
@@ -65,13 +88,59 @@ export default function createPlugin(options?: Options): Plugin {
 						),
 					]),
 				);
-			const obj = allIcons.reduce(
+
+			inMemoryCollections = allIcons.reduce(
 				(prev, [name, value]) => ({ ...prev, [name]: value }),
 				{},
 			);
-			console.log(obj);
 
-			return js`export default ${JSON.stringify(obj)};`;
+			return js`export default {};`;
+		},
+
+		async buildEnd() {
+			const usageRaw = fs.readFileSync(cacheFilename, {
+				encoding: "utf-8",
+				flag: "as+",
+			});
+			const usage = JSON.parse(usageRaw || "{}");
+
+			for (const pack of Object.keys(usage)) {
+				const usedIcons = usage[pack];
+				const collection = inMemoryCollections[pack];
+				if (!collection) continue;
+
+				const source = generateSprite(collection, usedIcons || []);
+
+				this.emitFile({
+					type: "asset",
+					fileName: `${pack}.svg`,
+					source,
+				});
+			}
+		},
+
+		configureServer(server) {
+			server.middlewares.use(async (req, res, next) => {
+				for (const pack of Object.keys(inMemoryCollections)) {
+					console.log(pack);
+					if (req.url === `/${pack}.svg`) {
+						const loaded = JSON.parse(
+							fs.readFileSync(cacheFilename, {
+								encoding: "utf8",
+								flag: "as+",
+							}) || "{}",
+						);
+						res.setHeader("Content-Type", "image/svg+xml");
+						const data = generateSprite(
+							inMemoryCollections[pack],
+							loaded[pack] || [],
+						);
+						return res.end(data);
+					}
+				}
+
+				return next();
+			});
 		},
 	};
 }
@@ -92,9 +161,20 @@ export function collectionHash(collections: IconifyJSON[]) {
 	return hash.digest("hex");
 }
 
+function generateSprite(packIcons: IconifyJSON, loaded: string[]) {
+	let str = `<svg xmlns="http://www.w3.org/2000/svg" style="display:none">\n`;
+	for (const icon of loaded) {
+		const data = packIcons.icons[icon];
+		const svg = iconToSVG(data, { height: 16, width: 16 });
+		str += `<symbol id="${icon}">${svg.body}</symbol>`;
+	}
+	str += `\n</svg>`;
+	return str;
+}
+
 async function tryGetHash(path: URL): Promise<string | void> {
 	try {
-		const text = await readFile(path, { encoding: "utf-8" });
+		const text = fs.readFileSync(path, { encoding: "utf-8" });
 		return text.split("\n", 3)[1].replace("// ", "");
 	} catch {
 		console.error("unable to get hash");
